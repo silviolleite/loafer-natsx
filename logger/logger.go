81b69e849644ea42f085b4471575fc5aa3f4d664
silvio.leite <silvio.leite@olist.com>
2026-02-12 17:51:07 -0300

    feature(logger): Add Debug log to the logger interfaces

--- logger/logger.go
+++ logger/logger.go
@@ -1,1 +1,22 @@
 package logger
+
+/*
+Logger is the minimal logging interface used by Processor.
+*/
+type Logger interface {
+	Info(msg string, args ...any)
+	Error(msg string, args ...any)
+	Debug(msg string, args ...any)
+}
+
+// NopLogger is a no-operation logger that silently discards all logged messages.
+type NopLogger struct{}
+
+// Debug logs a debug-level message with optional arguments, but the implementation discards the message and does nothing.
+func (l NopLogger) Debug(msg string, args ...any) {}
+
+// Error logs an error message with optional arguments, but the implementation discards the message and does nothing.
+func (NopLogger) Error(string, ...any) {}
+
+// Info logs an informational message with optional arguments, but the implementation discards the message and does nothing.
+func (NopLogger) Info(string, ...any) {}
